55373c0ce7495d459fad95e3fed503212296218a
LarsonKao <larsonkao0528@gmail.com>
2024-03-15 00:15:18 +0800

    add teacher update rspec

--- app/controllers/api/v1/teachers_controller.rb
+++ app/controllers/api/v1/teachers_controller.rb
@@ -15,10 +15,18 @@ module Api::V1
     end
 
     def update
+      teacher = current_user.teacher
+      return error_response(:not_found_teacher, {error: 'not found teacher'}) if teacher.nil?
+      teacher.update(update_params)
+      return error_response(:unprocessable_entity_result, {error: teacher.errors.full_messages.join(", ")}) if current_user.errors.present?
+
+      success_response
+    end
+
+    def destroy
       teqacher = current_user.teacher
       return error_response(:not_found, {error: 'not found teacher'}) if teqacher.nil?
-      teqacher.update(update_params)
-      return error_response(:unprocessable_entity_result, {error: teqacher.errors.full_messages.join(", ")}) if current_user.errors.present?
+      return error_response(:unprocessable_entity_result, {error: current_user.errors.full_messages}) if current_user.errors.present?
 
       success_response
     end
--- spec/controllers/teachers/update_spec.rb
+++ spec/controllers/teachers/update_spec.rb
@@ -7,6 +7,7 @@ RSpec.describe Teacher, type: :request do
   describe "PUT update" do
     path = "/api/v1/teachers"
     let(:teacher) { create(:teachers) }
+    let(:user) { create(:users) }
 
     context "when user is logged in" do
       let(:valid_token) { create(:access_token, resource_owner_id: teacher.user.id, application_id: client.id) }
@@ -27,6 +28,22 @@ RSpec.describe Teacher, type: :request do
         patch(path, headers: json_headers, params: params.to_json)
         teacher.reload
         expect(teacher.lab).to eq(params[:lab])
+      end
+    end
+
+    context "when user has no teacher" do
+      let(:valid_token) { create(:access_token, resource_owner_id: user.id, application_id: client.id) }
+
+      let(:params) do
+        {
+          lab: 'NEW FAB'
+        }
+      end
+
+      it "should return http status code 404" do
+        json_headers[:Authorization] = "Bearer #{valid_token.token}"
+        result = patch(path, headers: json_headers, params: params.to_json)
+        expect(result).to eq(404)
       end
     end
   end
