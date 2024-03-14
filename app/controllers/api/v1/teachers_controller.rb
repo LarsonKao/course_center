21987ab49db568afbfef917e93c1c8f77cace095
LarsonKao <larsonkao0528@gmail.com>
2024-03-15 00:35:09 +0800

    update rspec

--- app/controllers/api/v1/teachers_controller.rb
+++ app/controllers/api/v1/teachers_controller.rb
@@ -1,6 +1,13 @@
 module Api::V1
   class TeachersController < ApplicationController
+    before_action :current_teacher, only: [:update, :show, :destroy]
+
+    def show
+      success_response(current_user)
+    end
+
     def create
+      return error_response(:existing_teacher) if current_user.teacher.present?
       teacher = Teacher.new(create_params)
       current_user.teacher = teacher
       if teacher.save && current_user.save
@@ -15,24 +22,26 @@ module Api::V1
     end
 
     def update
-      teacher = current_user.teacher
-      return error_response(:not_found_teacher, {error: 'not found teacher'}) if teacher.nil?
-      teacher.update(update_params)
-      return error_response(:unprocessable_entity_result, {error: teacher.errors.full_messages.join(", ")}) if teacher.errors.present?
+      current_teacher.update(update_params)
+      return error_response(:unprocessable_entity_result, {error: current_teacher.errors.full_messages.join(", ")}) if current_teacher.errors.present?
 
       success_response
     end
 
     def destroy
-      teacher = current_user.teacher
-      return error_response(:not_found_teacher, {error: 'not found teacher'}) if teacher.nil?
-      teacher.destroy
-      return error_response(:unprocessable_entity_result, {error: current_user.errors.full_messages}) if teacher.errors.present?
+      current_teacher.destroy
+      return error_response(:unprocessable_entity_result, {error: current_teacher.errors.full_messages}) if current_teacher.errors.present?
 
       success_response
     end
 
     private
+
+    def current_teacher
+      @teacher = current_user.teacher
+      return head :not_found if @teacher.nil?
+      @teacher
+    end
 
     def create_params
       params.permit(:lab)
--- spec/controllers/teachers/create_spec.rb
+++ spec/controllers/teachers/create_spec.rb
@@ -7,6 +7,7 @@ RSpec.describe Teacher, type: :request do
   describe "POST create teacher" do
     path = "/api/v1/teachers"
     let(:user) { create(:users) }
+    let(:teacher) { create(:teachers)}
     let(:params) {{ lab: "LAB202" }}
     context "when user has no teacher" do
       let(:valid_token) { create(:access_token, resource_owner_id: user.id, application_id: client.id) }
@@ -23,6 +24,16 @@ RSpec.describe Teacher, type: :request do
         post(path, headers: json_headers, params: params.to_json)
         result = JSON.parse(response.body, symbolize_names: true)[:data]
         expect(result[:id]).to eq(user.teacher.id)
+      end
+    end
+
+    context "when user has no teacher" do
+      let(:valid_token) { create(:access_token, resource_owner_id: teacher.user.id, application_id: client.id) }
+      let(:expired_token) { create(:expired_token, resource_owner_id: teacher.user.id, application_id: client.id) }
+      it "should return 404" do
+        json_headers[:Authorization] = "Bearer #{valid_token.token}"
+        result = post(path, headers: json_headers, params: params.to_json)
+        expect(result).to eq(400)
       end
     end
   end
