2039ce7912905c5347b5f89a198d14c5e91ddb79
LarsonKao <larsonkao0528@gmail.com>
2024-03-16 01:16:24 +0800

    optimize user rspec

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-
-  mount Rswag::Ui::Engine => '/api-docs'
-  mount Rswag::Api::Engine => '/api-docs'
   namespace :api do
     namespace :v1 do
       resource :users, only: %i[show create destroy update] do
--- spec/controllers/users/update_spec.rb
+++ spec/controllers/users/update_spec.rb
@@ -13,20 +13,23 @@ RSpec.describe User, type: :request do
       let(:expired_token) { create(:expired_token, resource_owner_id: user.id, application_id: client.id) }
       let(:params) do
         {
-          name: 'John Doe'
+          name: 'HIHI'
         }
       end
-      it "should return http status code 200" do
+      it "should return http status code 200 when all right" do
         json_headers[:Authorization] = "Bearer #{valid_token.token}"
         result = patch(path, headers: json_headers, params: params.to_json)
+        user.reload
         expect(result).to eq(200)
+        expect(user.name).to eq(params[:name])
       end
 
-      it "should update the user name" do
-        json_headers[:Authorization] = "Bearer #{valid_token.token}"
-        patch(path, headers: json_headers, params: params.to_json)
+      it "should return http status code 401 when token is invalid" do
+        json_headers[:Authorization] = "Bearer #{expired_token.token}"
+        result = patch(path, headers: json_headers, params: params.to_json)
         user.reload
-        expect(user.name).to eq(params[:name])
+        expect(result).to eq(401)
+        expect(user.name).not_to eq(params[:name])
       end
     end
   end
